da6c69706d34121d43b7bef839af6a7c529dfb98
Alexander <webisforce@gmail.com>
2022-01-19 17:26:48 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -176,7 +176,7 @@ module.exports = {
         docs: {
           routeBasePath: "/",
           sidebarPath: require.resolve("./sidebars.js"),
-          editUrl: "https://github.com/quarkly/developer-docs/",
+          editUrl: "https://github.com/quarkly/developer-docs/blob/main/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
